095974d09dd1de956bac60c1e9045b15e5dc7fd0
lunar moon <49crehbgr@gmail.com>
2023-01-05 16:52:39 +0900

    [unrated] Title: 귤 고르기, Time: 18.78 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -1,18 +1,28 @@
 import collections
 
 def solution(k, tangerine):
-    answer = 0
-    hashmap = collections.defaultdict(int)
+    # sol 1
+#     answer = 0
+#     hashmap = collections.defaultdict(int)
     
-    for i in tangerine:
-        hashmap[i] += 1
+#     for i in tangerine:
+#         hashmap[i] += 1
     
-    hashmap = sorted(hashmap.items(), key=lambda x: x[1], reverse=True)
+#     hashmap = sorted(hashmap.items(), key=lambda x: x[1], reverse=True)
     
-    for key, val in hashmap:
-        if k <= 0: break
-        k -= val
-        answer += 1
+#     for key, val in hashmap:
+#         if k <= 0: break
+#         k -= val
+#         answer += 1
         
     
+#     return answer
+    # sol 2
+    answer = 0
+    c = collections.Counter(tangerine)
+    sort_list = sorted(c.values(), reverse=True)
+    for v in sort_list:
+        k -= v
+        answer += 1
+        if k <= 0: break
     return answer
