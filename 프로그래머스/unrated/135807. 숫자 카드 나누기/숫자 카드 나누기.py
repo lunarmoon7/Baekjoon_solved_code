79b81dca82ec36941d5a66f12a61c7a9aec08b8b
lunar moon <49crehbgr@gmail.com>
2023-01-08 16:10:24 +0900

    [unrated] Title: 숫자 카드 나누기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.py
+++ 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -1,40 +1,37 @@
 from functools import reduce
 import math
-# def lcm(a, b):
-#     return (a * b) // math.gcd(a, b)
+def isMod(arr, gcd):
+    for num in arr:
+        if num % gcd == 0: return True
+    return False
 def solution(arrayA, arrayB):
     answer = 0
-    isMod_a, isMod_b = False, False
+    n = len(arrayA)
     gcd_a, gcd_b = arrayA[0], arrayB[0]
-    for i in range(1, len(arrayA)):
+    for i in range(1, n):
         gcd_a = math.gcd(gcd_a, arrayA[i])
         gcd_b = math.gcd(gcd_b, arrayB[i])
-    # lcm_a = reduce(lambda x, y: x * y, arrayA) // gcd_a
-    # lcm_b = reduce(lambda x, y: x * y, arrayB) // gcd_b
-    if gcd_a == 1:
-        for num in arrayA:
-            if num % gcd_b == 0:
-                return 0
-        return gcd_b
-    elif gcd_b == 1:
-        for num in arrayB:
-            if num % gcd_a == 0:
-                return 0
-        return gcd_a
-    else:
-        for num in arrayB:
-            if num % gcd_a == 0:
-                isMod_a = True
-                break
-        for num in arrayA:
-            if num % gcd_b == 0:
-                isMod_b = True
-                break
-        if isMod_a and isMod_b: return 0
-        if isMod_a:
-            if not isMod_b: return gcd_b
-        if isMod_b:
-            if not isMod_a: return gcd_a
-        if not isMod_a and not isMod_b: return max(gcd_a, gcd_b)
+        
+    if gcd_a != 1:
+        if not isMod(arrayB, gcd_a):
+            answer = gcd_a
+    if gcd_b != 1:
+        if not isMod(arrayA, gcd_b):
+            answer = max(answer, gcd_b)
+    # if gcd_a == 1:
+    #     if isMod(arrayA, gcd_b): return 0
+    #     return gcd_b
+    # elif gcd_b == 1:
+    #     if isMod(arrayB, gcd_a): return 0
+    #     return gcd_a
+    # else:
+    #     isMod_a = isMod(arrayB, gcd_a)
+    #     isMod_b = isMod(arrayA, gcd_b)
+    #     if isMod_a and isMod_b: return 0
+    #     if isMod_a:
+    #         if not isMod_b: return gcd_b
+    #     if isMod_b:
+    #         if not isMod_a: return gcd_a
+    #     if not isMod_a and not isMod_b: return max(gcd_a, gcd_b)
         
     return answer
