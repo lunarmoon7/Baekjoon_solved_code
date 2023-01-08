202087bc6e76a172de45db4caa837eca84e1ce89
lunar moon <49crehbgr@gmail.com>
2023-01-08 20:46:00 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
+++ 프로그래머스/lv2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
@@ -7,13 +7,14 @@ def solution(cards):
     for start in range(n):
         if not isPicked[start]:
             helper(cards, isPicked, start, [])
-    ret.sort(reverse=True)
-    answer = 0 if len(ret) == 1 else ret[0] * ret[1]
+    ret.sort(key=lambda x: len(x), reverse=True)
+    answer = 0 if len(ret) == 1 else len(ret[0]) * len(ret[1])
     return answer
 def helper(cards, isPicked, idx, group):
     global ret
     if isPicked[idx]:
-        ret.append(len(group))
+        # ret.append(len(group))
+        ret.append(group)
         return
     isPicked[idx] = True
     helper(cards, isPicked, cards[idx] - 1, group + [cards[idx]])
