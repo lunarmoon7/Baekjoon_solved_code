6408b5d0cec18c745f641f97803e2bf6482b8f69
lunar moon <49crehbgr@gmail.com>
2023-01-12 15:51:33 +0900

    [level 2] Title: 양궁대회, Time: 14.89 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
@@ -0,0 +1,47 @@
+import copy
+max_diff = 0
+answer = []
+def calcDiff(info, shots):
+    apeach_sum, rion_sum = 0, 0
+    for i in range(11):
+        if (info[i], shots[i]) == (0, 0): continue
+        if info[i] >= shots[i]:
+            apeach_sum += 10 - i
+        else:
+            rion_sum += 10 - i
+    return rion_sum - apeach_sum
+def dfs(n, info, shots, idx):
+    global max_diff, answer
+    if idx == 11:
+        if n != 0:
+            shots[10] = n
+        score_diff =calcDiff(info, shots)
+        if score_diff <= 0:
+            return
+        ret = copy.deepcopy(shots)
+        if score_diff > max_diff:
+            max_diff = score_diff
+            answer = [ret]
+            return
+        if score_diff == max_diff:
+            answer.append(ret)
+        return 
+    if info[idx] < n:
+        shots.append(info[idx] + 1)
+        dfs(n - info[idx] - 1, info, shots, idx + 1)
+        shots.pop()
+    
+    shots.append(0)
+    dfs(n, info, shots, idx + 1)
+    shots.pop()
+
+def solution(n, info):
+    global max_diff, answer
+    dfs(n, info, [], 0)
+    if not answer: return [-1]
+    answer.sort(key = lambda x : x[::-1], reverse=True)
+    return answer[0]
+
+    
+
+    
